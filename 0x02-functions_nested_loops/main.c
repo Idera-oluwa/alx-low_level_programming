ff91a7d3da6275747bf3f920500a51dc84beb099
Isa <ideraoluwaisa2003@gmail.com>
2022-09-13 11:33:17 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,2 @@
+In the following examples, the main.c files are shown as examples. You can use them to test your functions,
+   We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
